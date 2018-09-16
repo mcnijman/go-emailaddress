23abd3bf4256f5b7c2273011146c55015d79359c
Thijs Nijman <mcnijman@gmail.com>
2018-09-16 13:01:23 +0200

    Add copyright header

--- emailaddress_test.go
+++ emailaddress_test.go
@@ -1,3 +1,8 @@
+// Copyright 2018 The go-emailaddress AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
 package emailaddress
 
 import (
